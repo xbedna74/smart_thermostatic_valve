d6b69b71c0b597957d63eba0a142eb71cb7c71b4
xbedna74 <xbedna74@stud.fit.vutbr.cz>
2021-02-18 14:10:36 +0100

    restructure and gui

--- restApi.py
+++ restApi.py
@@ -2,8 +2,14 @@ from http.server import BaseHTTPRequestHandler
 
 from thermostaticValve import *
 
+"""
+	Handler for http requests
+	@class
+	
+"""
 class RequestHandler(BaseHTTPRequestHandler):
-	def _send_response(self, payload, code):
+	#function for response sending
+	def _send_response(self, code, payload=''):
 		payload = str(payload)
 		self.protocol_version = "HTTP/1.1"
 		self.send_response(code)
@@ -14,6 +20,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 		if (len(payload) > 0):
 			self.wfile.write(bytes(payload, "ascii"))
 
+	#GET handler
 	def do_GET(self):
 
 		print(self.path)
@@ -27,11 +34,12 @@ class RequestHandler(BaseHTTPRequestHandler):
 					if (path_parts[3] == "position"):
 						this_valve.controlValve()
 						this_valve.setCurrentPosition()
-						self._send_response(this_valve.getPosition(), 200)
+						this_valve.getPosition()
+						self._send_response(200, this_valve.getPosition())
 				else:
-					self._send_response('', 404)
+					self._send_response(404)
 
-
+	#POST handler
 	def do_POST(self):
 
 		print(self.path)
@@ -43,26 +51,27 @@ class RequestHandler(BaseHTTPRequestHandler):
 				if (len(path_parts) == 2):
 					new_valve = ThermostaticValve()
 					print("New valve registered: " + str(new_valve.getId()))
-					self._send_response(str(new_valve.getId()), 201)
+					self._send_response(201, str(new_valve.getId()))
 
 				elif (path_parts[2].isdigit() and path_parts[3] == "current-temperature"):#add control if temperature is float (fifth part(forth index))
 					this_valve = ThermostaticValve.getValve(path_parts[2])
 
 					if (len(path_parts) < 5):
-						self._send_response('', 204)#no content
+						self._send_response(204)#no content
 					elif (this_valve is not None):
 						print("Valves " + str(path_parts[2]) + " temperature is " + str(path_parts[4]))
 						this_valve.setCurrentTemperature(float(path_parts[4]))
-						self._send_response('', 200)
+						self._send_response(200)
 					else:
-						self._send_response('', 404)#id not found
+						self._send_response(404)#id not found
 
-
+	#PUT hanlder
 	def do_PUT(self):
 		self.do_POST()
 
+	#DELELTE handler
 	def do_DELETE(self):
-		
+
 		print(self.path)
 
 		path_parts = self.path.split('/')
@@ -72,13 +81,6 @@ class RequestHandler(BaseHTTPRequestHandler):
 			if (path_parts[1] == "radiator-valve"):
 				if (path_parts[2].isdigit()):
 					if (ThermostaticValve.removeValve(path_parts[2])):
-						self._send_response('', 200)
+						self._send_response(200)
 					else:
-						self._send_response('', 404)
-					#this_valve = ThermostaticValve.getValve(path_parts[2])
-
-					#if (this_valve is not None):
-					#	ThermostaticValve.removeValve(int(path_parts[2]))
-					#	self._send_response('', 200)
-					#else:
-					#	self._send_response('', 404)
+						self._send_response(404)
--- thermostaticValve.py
+++ thermostaticValve.py
@@ -1,5 +1,10 @@
 import time
 
+
+"""
+	Thermostatic valve
+	@class
+"""
 class ThermostaticValve:
 	week_prg_def = [None] * 7
 
@@ -13,6 +18,7 @@ class ThermostaticValve:
 	valves = []
 
 
+	#constructor
 	def __init__(self):
 		self.id = ThermostaticValve.free_id
 		print("New valve created: " + str(self.id))
@@ -32,13 +38,22 @@ class ThermostaticValve:
 		self.open_window = False
 
 		self.boost = False
-		self.mode = 0
+		self.mode = 1
 
 		self.falling = False
 		self.recovering = False
 
+		self.alias = ''
+
 		ThermostaticValve.valves.append(self)
 
+	#returns ids of all existing valves
+	@staticmethod
+	def getIds():
+		return ThermostaticValve.ids
+
+	#returns new id for new valve
+	@staticmethod
 	def getNewId():
 		ThermostaticValve.ids.append(ThermostaticValve.free_id)
 		ThermostaticValve.ids.sort()
@@ -51,27 +66,41 @@ class ThermostaticValve:
 		if (prev_id == ThermostaticValve.free_id):
 			ThermostaticValve.free_id = len(ThermostaticValve.ids)
 
+	#returns valve id
 	def getId(self):
 		return self.id
 
+	def setAlias(self, alias):
+		self.alias = alias
+
+	def getAlias(self):
+		return self.alias
+
+	#sets temperature
 	def setTemperature(self, tmp):
-		self.tmp = tmp
+		self.casual_tmp = tmp
 
+	#returns set temperature
 	def getTemperature(self):
-		return self.tmp
+		return self.casual_tmp
 
+	#returns week program temperature at given time
 	def getWeekProgramTemperature(self, day, hour):
-		return self.week_prg[day[hour]]
+		return self.week_prg[day][hour]
 
-	def setWeekProgram(self, week_prg):
-		self.week_prg = week_prg
+	#sets week program of valve
+	def setWeekProgramTemperature(self, day, hour, tmp):
+		self.week_prg[day][hour] = tmp
+	#	self.week_prg = week_prg
 
+	#sets position of valve
 	def setPosition(self, position):
 		self.position = position
 
 	def getPosition(self):
 		return self.position
 
+	#sets current temperature and saves it to list for future use
 	def setCurrentTemperature(self, tmp):
 		self.current_temperature = tmp
 
@@ -86,9 +115,11 @@ class ThermostaticValve:
 			self.temperatures[self.temperatures_index] = tmp
 			self.temperatures_index += 1
 
+	#returns current temperature
 	def getCurrentTemperature(self):
 		return self.current_temperature
 
+	#sets current position and saves it to list for future use
 	def setCurrentPosition(self):
 		pos = self.getPosition()
 
@@ -103,7 +134,8 @@ class ThermostaticValve:
 			self.positions[self.positions_index] = pos
 			self.positions_index += 1
 
-
+	#helping function to get index of day
+	@staticmethod
 	def getDayIndex(day):
 		return {
 			'mon' : 0,
@@ -115,7 +147,7 @@ class ThermostaticValve:
 			'sun' : 6,
 		}[day.lower()]
 
-
+	#returns temperature according to selected mode
 	def getDesiredTemperature(self):
 		if (self.mode == 0):
 			return self.week_prg[ThermostaticValve.getDayIndex(time.strftime("%a"))][int(time.strftime("%H")) - 1]
@@ -124,13 +156,21 @@ class ThermostaticValve:
 		elif (self.mode == 2):
 			return self.eco_tmp
 
-	#return valve object according to id
+	def getMode(self):
+		return self.mode
+
+	def setMode(self, mode):
+		self.mode = mode
+
+	#returns valve object by id
+	@staticmethod
 	def getValve(id):
-		if (id.isdigit()):
+		if (isinstance(id, str) and id.isdigit()) or (isinstance(id, int)):
 			for valve in ThermostaticValve.valves:
 				if (int(id) == valve.getId()):
 					return valve
 					break
+
 		return None
 
 	#controls position of valve
@@ -171,23 +211,27 @@ class ThermostaticValve:
 			self.setPosition(1.0)
 			self.recovering = False
 			self.falling = True
-		#elif (difference_in_tmp > 1):
-		#	self.setPosition(4.0)
-		#elif (difference_in_tmp > 0.5):
-		#	self.setPosition(3.0)
-		#elif (difference_in_tmp > 0):
-		#	self.setPosition(2.0)
-		#else:
-		#	self.setPosition(1.0)
+
+		"""elif (difference_in_tmp > 1):
+			self.setPosition(4.0)
+		elif (difference_in_tmp > 0.5):
+			self.setPosition(3.0)
+		elif (difference_in_tmp > 0):
+			self.setPosition(2.0)
+		else:
+			self.setPosition(1.0)"""
 
 		if (self.temperatures_index > 10):
 			tmps = self.temperatures[:self.temperatures_index + 1 if self.temperatures_index != 100 else 100]
 			poss = self.positions[:self.positions_index + 1 if self.positions_index != 50 else 50]
 
 	#sets boost mode on
-	def setBoostMode(self, t=time.time()):
+	def setBoostModeOn(self, t=time.time()):
 		self.boost = True
 		self.boost_time = t
+
+	def setBoostModeOff(self):
+		self.boost = False
 
 	#returns True if boost mode is still on, otherwise False
 	def getBoostMode(self, t=time.time()):
@@ -199,6 +243,8 @@ class ThermostaticValve:
 
 		return False
 
+	#detects open window by quick difference in temperature in humidity
+	#TODO
 	def openWindowDetection(self):
 		if (self.temperatures_index == 100):
 			tmps = self.temperatures[-5:]
@@ -207,6 +253,8 @@ class ThermostaticValve:
 
 		return False
 
+	#deletes valve from system
+	@staticmethod
 	def removeValve(id):
 		del_valve = ThermostaticValve.getValve(id)
 
@@ -214,7 +262,7 @@ class ThermostaticValve:
 			ThermostaticValve.valves.remove(del_valve)
 			del del_valve
 			ThermostaticValve.ids.remove(int(id))
-			ThermostaticValve.getNewId()
+			#ThermostaticValve.getNewId()
 			print("Valve " + id + " deleted")
 			return True
 		else:
